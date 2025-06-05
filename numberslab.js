3e1d15e0b401b0e0aee1c998e1e9318da55fd130
StormieMurphy <stolenmurphy@gmail.com>
2025-06-04 21:38:17 -0500

    Completed Lab Exercise - Numbers

---
+++ numberslab.js
@@ -0,0 +1,21 @@
+// Task 1
+const literalA = 34_038;
+const literalB = 45_838.8734;
+const literalC = 0b0110_0010_1011;
+const literalD = 0x17_4C_0;
+const literalE = -895_612;
+
+console.log(literalA);
+console.log(literalB);
+console.log(literalC);
+console.log(literalD);
+console.log(literalE);
+
+// Task 2
+const hexadecimalValue = 0x1D;
+const binaryValue = 0b11101;
+const octalValue = 0o35;
+
+console.log(hexadecimalValue);
+console.log(binaryValue);
+console.log(octalValue);
