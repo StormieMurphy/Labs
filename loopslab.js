b92cca776df7f3d1cec1757985ef46608e9322a9
StormieMurphy <stolenmurphy@gmail.com>
2025-06-16 19:18:51 -0500

    Completed Lab Exercise - Loops

---
+++ loopslab.js
@@ -0,0 +1,23 @@
+// Task 1
+
+function numberCount () {
+    counter = 5;
+    while (counter < 26) {
+        console.log(counter);
+        counter++;
+    }
+}
+
+numberCount();
+
+// Task 2
+
+function checkDivisible () {
+    for (counter = 0; counter <= 30; counter ++){
+        if (counter % 3 == 0) {
+            console.log(counter)
+        }
+    }
+}
+
+checkDivisible();
